a2c52a9341e0a022e5c5bd0d649c3837e991ef6e
Marc Mintel <marc@mintel.me>
2019-12-30 14:05:26 +0100

    refactor: navItem

--- packages/baretheme-theme/src/components/nav.js
+++ packages/baretheme-theme/src/components/nav.js
@@ -2,10 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { List, Bar, Stack } from '@baretheme/ui';
 import Types from '../types';
+import useLocalePath from '../hooks/locale-path';
 
 import LocalePath from './locale-path';
 import Link from './link';
 
+const NavItem = ({
+  item, text, title, path,
+}) => {
+  const ItemComponent = item;
+  const TextComponent = text;
+  const localePath = useLocalePath(path);
+  return (
+    <ItemComponent>
+      <TextComponent as={Link} to={localePath}>
+        { title }
+      </TextComponent>
+    </ItemComponent>
+  );
+};
+
+NavItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
+  item: PropTypes.elementType.isRequired,
+  text: PropTypes.elementType.isRequired,
+};
 
 const Nav = ({
   align, flush, type, items,
@@ -14,15 +36,13 @@ const Nav = ({
     return (
       <Bar align={align} flush={flush}>
         {items && items.map((item) => (
-          <LocalePath path={item.slug} key={item.id}>
-            {(localePath) => (
-              <Bar.Item>
-                <Bar.ItemText as={Link} to={localePath}>
-                  { item.title }
-                </Bar.ItemText>
-              </Bar.Item>
-            )}
-          </LocalePath>
+          <NavItem
+            item={Bar.Item}
+            text={Bar.ItemText}
+            key={item.id}
+            title={item.title}
+            path={item.slug}
+          />
         ))}
       </Bar>
     );
